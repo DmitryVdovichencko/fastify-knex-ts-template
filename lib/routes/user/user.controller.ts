f809c7f85a81b56a97e646c77ed87ac80c64a913
Dmitry Vdovichenko <dmitry.vdovichencko@gmail.com>
2025-03-15 13:47:13 +0500

    added routes

--- lib/routes/user/user.controller.ts
+++ lib/routes/user/user.controller.ts
@@ -1,14 +1,6 @@
-import { FastifyInstance, FastifyPluginAsync, FastifyPluginOptions, FastifyReply, FastifyRequest } from "fastify";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { v4 as uuid } from 'uuid';
 import { hash } from 'node:crypto';
-import { schema } from "./schema";
-
-export const userList: FastifyPluginAsync = async (server: FastifyInstance, options: FastifyPluginOptions) => {
-	server.get("/api/user/list", schema, async (req: FastifyRequest, reply: FastifyReply) => {
-		const users = await server.knex.select('*').from('user').limit(100).offset(0)
-		reply.code(200).send({ list: users });
-	})
-};
 
 export const registerUser = async (request: FastifyRequest, reply: FastifyReply, server: FastifyInstance) => {
 	const { username, password } = request.body as { username: string, password: string };
@@ -34,4 +26,10 @@ export const registerUser = async (request: FastifyRequest, reply: FastifyReply,
 			return reply.status(500).send({ error: error, message: "Failed to create user" });
 	}
 
+};
+
+export const getUserList = async (request: FastifyRequest, reply: FastifyReply, server: FastifyInstance) => {
+		const users = await server.knex.select('*').from('user').limit(100).offset(0);
+
+		reply.code(200).send({ list: users });
 };
--- lib/routes/user/user.route.ts
+++ lib/routes/user/user.route.ts
@@ -1,10 +1,13 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import { registerUser } from "./user.controller";
-import { registerUserSchema } from "./schema";
+import { getUserList, registerUser } from "./user.controller";
+import { registerUserSchema, userListSchema } from "./schema";
 
 // User routes
 export const userRoute = async (app: FastifyInstance) => {
   app.post("/register", registerUserSchema, async (req: FastifyRequest, reply: FastifyReply) =>
     registerUser(req, reply, app)
+  );
+	app.get("/list", userListSchema, async (req: FastifyRequest, reply: FastifyReply) =>
+    getUserList(req, reply, app)
   );
 };
